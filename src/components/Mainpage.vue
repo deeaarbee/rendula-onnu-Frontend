fb027a61685ff2c570d2c4ad1e51db259e906777
karkeykk <karthiorton@gmail.com>
2018-06-15 18:06:59 +0530

    Image test

--- src/components/Mainpage.vue
+++ src/components/Mainpage.vue
@@ -5,11 +5,11 @@
     <br>
     <div class="columns is-gapless is-multiline is-mobile">
       <div class="column" @click="trueclick">
-        <!--<img src="../assets/logo.png" alt="">-->
+        <img v-bind:src="img1" alt="">
         <h1 class="title is-1">{{ option1 }}</h1>
       </div>
       <div class="column" @click="falseclick">
-        <!--<img src="../assets/logo.png" alt="">-->
+        <img v-bind:src="img2" alt="">
         <h1 class="title is-1" >{{ option2 }}</h1>
       </div>
     </div>
@@ -24,21 +24,27 @@
       props:["root_url"],
       data(){
           return{
-            options:["Kp","canteen","Kp","snh","Narayanasamy","geetha","Green benCh","blue shed",
-              "Senior","junior","Nss,nso,yrc or ctf","SA","Techofes","kurukshetra","main gate",
-              "kotur gate","Spartns","twisters","Ice tea","cold coffee"],
+            images: ["../assets/KP.jpg","../assets/Canteen.jpg"],
+            options:["Cafeteria","canteen","Kp","Snh","Narayanasamy","Geetha","Green bench","Blue shed",
+              "Senior","Junior","Nss,nso,yrc or ctf","SA","Techofes","Kurukshetra","Main gate",
+              "Kottur gate","Spartans","Twisters","Ice tea","Cold coffee"],
             answers:[],
             value:0,
             iter1:0,
             iter2:1,
+            img1:"",
+            img2:"",
             option1:"",
             option2:"",
         }
       },
       mounted(){
-          this.option1 = this.options[this.iter1];
+        this.option1 = this.options[this.iter1];
         this.option2 = this.options[this.iter2];
-        this.value = 10
+        this.img1 = this.images[this.iter1];
+        this.img2 = this.images[this.iter2];
+        this.value = 10;
+        console.log(this.img1);
       },
       updated(){
 
@@ -47,20 +53,34 @@
           trueclick : function () {
             this.value +=10;
             if(this.value == 110){
+              this.answers.push(true);
               console.log(this.answers)
               this.$router.push('/thankyou')
             }
             this.answers.push(true);
+            
+            this.$http.post('https://jsonplaceholder.typicode.com/posts',{
+              title: this.option1,
+              body: this.option2,
+            }).then(function(data){
+              console.log(data);
+              });
             this.update_questions();
-
           },
         falseclick: function () {
           this.value +=10;
           if(this.value == 110){
+            this.answers.push(false);
             console.log(this.answers)
             this.$router.push('/thankyou')
           }
           this.answers.push(false);
+          this.$http.post('https://jsonplaceholder.typicode.com/posts',{
+              title: this.option1,
+              body: this.option2,
+          }).then(function(data){
+              console.log(data);
+              });
           this.update_questions();
 
 
@@ -70,7 +90,8 @@
           this.iter2 += 2;
           this.option1 = this.options[this.iter1];
           this.option2 = this.options[this.iter2];
-
+          this.img1 = this.images[this.iter1];
+          this.img2 = this.images[this.iter2];
         }
       }
 
